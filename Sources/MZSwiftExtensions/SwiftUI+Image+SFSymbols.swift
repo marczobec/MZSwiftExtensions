523fa369aa3ddf44a2114134a219bccc62d8830b
Marc Zobec <kontakt@marczobec.de>
2022-10-11 15:53:50 +0200

    add image+sfsymbols extension

---
+++ Sources/MZSwiftExtensions/SwiftUI+Image+SFSymbols.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+public extension Image {
+    static let checkmark = Image(systemName: "checkmark")
+    static let checkmarkCircle = Image(systemName: "checkmark.circle")
+    static let exclamationmarkTriangle = Image(systemName: "exclamationmark.triangle")
+    static let house = Image(systemName: "house")
+    static let listBullet = Image(systemName: "list.bullet")
+    static let rosette = Image(systemName: "rosette")
+    static let squareAndPencil = Image(systemName: "square.and.pencil")
+}

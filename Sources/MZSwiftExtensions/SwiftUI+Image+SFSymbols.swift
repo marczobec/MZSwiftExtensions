b4997e0b3d97900b56f3993afcbf241880b424a5
Marc Zobec <kontakt@marczobec.de>
2022-10-13 08:46:09 +0200

    provide string constants for sf symbol images

--- Sources/MZSwiftExtensions/SwiftUI+Image+SFSymbols.swift
+++ Sources/MZSwiftExtensions/SwiftUI+Image+SFSymbols.swift
@@ -1,12 +1,28 @@
 import SwiftUI
 
 public extension Image {
-    static let checkmark = Image(systemName: "checkmark")
-    static let checkmarkCircle = Image(systemName: "checkmark.circle")
-    static let exclamationmarkTriangle = Image(systemName: "exclamationmark.triangle")
-    static let house = Image(systemName: "house")
-    static let listBullet = Image(systemName: "list.bullet")
-    static let plus = Image(systemName: "plus")
-    static let rosette = Image(systemName: "rosette")
-    static let squareAndPencil = Image(systemName: "square.and.pencil")
+    static let checkmarkName = "checkmark"
+    static let checkmark = Image(systemName: Self.checkmarkName)
+
+    static let checkmarkCircleName = "checkmark.circle"
+    static let checkmarkCircle = Image(systemName: Self.checkmarkCircleName)
+
+    static let exclamationmarkTriangleName = "exclamationmark.triangle"
+    static let exclamationmarkTriangle = Image(systemName: Self.exclamationmarkTriangleName)
+
+    static let houseName = "house"
+    static let house = Image(systemName: Self.houseName)
+
+    static let listBulletName = "list.bullet"
+    static let listBullet = Image(systemName: Self.listBulletName)
+
+    static let plusName = "plus"
+    static let plus = Image(systemName: Self.plusName)
+
+    static let rosetteName = "rosette"
+    static let rosette = Image(systemName: Self.rosetteName)
+
+    static let squareAndPencilName = "square.and.pencil"
+    static let squareAndPencil = Image(systemName: Self.squareAndPencilName)
+
 }

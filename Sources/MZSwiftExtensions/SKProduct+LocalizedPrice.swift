edc7942e7b197571b03dc0eeb6cf0b38cd91888b
Marc Zobec <kontakt@marczobec.de>
2022-10-14 21:44:06 +0200

    add skproduct extension

---
+++ Sources/MZSwiftExtensions/SKProduct+LocalizedPrice.swift
@@ -0,0 +1,12 @@
+import StoreKit
+
+public extension SKProduct {
+
+    var localizedPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = priceLocale
+        return formatter.string(from: price) ?? ""
+    }
+
+}

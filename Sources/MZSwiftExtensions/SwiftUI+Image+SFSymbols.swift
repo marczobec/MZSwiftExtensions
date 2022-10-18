b574aa434600df34712cf739910463dcba094e96
Marc Zobec <kontakt@marczobec.de>
2022-10-18 10:27:33 +0200

    add icloud.slash icon

--- Sources/MZSwiftExtensions/SwiftUI+Image+SFSymbols.swift
+++ Sources/MZSwiftExtensions/SwiftUI+Image+SFSymbols.swift
@@ -5,37 +5,40 @@ import SwiftUI
 
 public extension Image {
     static let arrowUpAndDownName = "arrow.up.arrow.down"
-    static let arrowUpAndDown = Image(systemName: Self.arrowUpAndDownName)
+    static let arrowUpAndDown = Image(systemName: arrowUpAndDownName)
 
     static let checkmarkName = "checkmark"
-    static let checkmark = Image(systemName: Self.checkmarkName)
+    static let checkmark = Image(systemName: checkmarkName)
 
     static let checkmarkCircleName = "checkmark.circle"
-    static let checkmarkCircle = Image(systemName: Self.checkmarkCircleName)
+    static let checkmarkCircle = Image(systemName: checkmarkCircleName)
 
     static let exclamationMarkTriangleName = "exclamationmark.triangle"
-    static let exclamationMarkTriangle = Image(systemName: Self.exclamationMarkTriangleName)
+    static let exclamationMarkTriangle = Image(systemName: exclamationMarkTriangleName)
 
     static let houseName = "house"
-    static let house = Image(systemName: Self.houseName)
+    static let house = Image(systemName: houseName)
 
     static let iCloudAndArrowUpName = "icloud.and.arrow.up"
-    static let iCloudAndArrowUp = Image(systemName: Self.iCloudAndArrowUpName)
+    static let iCloudAndArrowUp = Image(systemName: iCloudAndArrowUpName)
+
+    static let iCloudSlashName = "icloud.slash"
+    static let iCloudSlash = Image(systemName: iCloudSlashName)
 
     static let listBulletName = "list.bullet"
-    static let listBullet = Image(systemName: Self.listBulletName)
+    static let listBullet = Image(systemName: listBulletName)
 
     static let person3Name = "person.3"
-    static let person3 = Image(systemName: Self.person3Name)
+    static let person3 = Image(systemName: person3Name)
 
     static let plusName = "plus"
-    static let plus = Image(systemName: Self.plusName)
+    static let plus = Image(systemName: plusName)
 
     static let rosetteName = "rosette"
-    static let rosette = Image(systemName: Self.rosetteName)
+    static let rosette = Image(systemName: rosetteName)
 
     static let squareAndPencilName = "square.and.pencil"
-    static let squareAndPencil = Image(systemName: Self.squareAndPencilName)
+    static let squareAndPencil = Image(systemName: squareAndPencilName)
 
 }
 
